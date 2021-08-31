e77c1367f81e99090e4fedfc5b1dce6085c00cae
y-mansour <youssefmansour@aucegypt.edu>
2021-08-31 16:39:56 +0000

    initial commit

---
+++ imagenet/config.py
@@ -0,0 +1,11 @@
+#Path to original dataset
+original_path='/workspace/media/hdd1/image_net_mini/imagenet-mini/train/'
+
+#Path to processed dataset
+data_path='/workspace/media/hdd1/image_net_mini/dataset2/'
+
+#Path to store trained models
+models_path= "/workspace/media/hdd1/image_net_mini/trained_models2/"
+
+#Path to store tensor board data
+logs_path= './logs/'
---
+++ ssid/config.py
@@ -0,0 +1,11 @@
+#Path to original dataset
+original_path='/workspace/media/hdd1/SSID/SIDD_Small_sRGB_Only/Data/'
+
+#Path to processed dataset
+data_path='/workspace/media/hdd1/SSID/dataset/'
+
+#Path to store trained models
+models_path= "/workspace/media/hdd1/SSID/trained_models/"
+
+#Path to store tensor board data
+logs_path= './logs/'
