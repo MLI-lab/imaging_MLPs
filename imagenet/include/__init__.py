f655aa4dea77f8017ece54fe4490efeed398f6df
y-mansour <youssefmansour@aucegypt.edu>
2021-09-03 09:44:27 +0000

    update results

--- imagenet/include/__init__.py
+++ imagenet/include/__init__.py
@@ -4,5 +4,9 @@ from .unet import *
 from .mixer_noU import *
 from .mixer_noSkip import *
 
+from .mixer_new128 import *
 from .mixer_new96 import *
 from .mixer_new64 import *
+
+from .mixer_mini128 import *
+from .mixer_shared128 import *
